b6d964e51e46cd5f297c1dba4c78df99f7204496
Rahul Vikram <vikramr@oregonstate.edu>
2025-01-24 18:42:23 -0800

    practice class writing

---
+++ learning/LINQtest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    internal class LINQtest
+    {
+        public static void PrintInts(int length)
+        {
+            int[] numbers = new int[length]; // array of length length
+
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] = i + 1;
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+    }
+
+    LINQtest.PrintInts(5);
+}
+
---
+++ learning/Payment.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace learning
+//{
+//    // internal: only accessible within the same project
+//    // abstract: cannot be instantiated
+//    internal abstract class Payment
+//    {
+//        public abstract string platform { get; } // must be implemented by subclasses
+//        private abstract string securityHash;
+//    }
+//}
